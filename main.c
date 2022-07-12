c2f70072fc0c47ec8e9d6e6d613112d33905c610
konstnty <konstanty.golda@gmail.com>
2022-07-12 15:17:32 +0200

    rewritten and fixed bugs

--- main.c
+++ main.c
@@ -1,67 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#include <curses.h>
+#include <stdlib.h>
+#include <ncurses.h>
 #include <unistd.h>
 
-// kbhit only in conio.h? not anymore
-// tbh still some problems with, it catches the input but doesnt act on it
-int kbhit() {
-    struct timeval tv;
-    fd_set fds;
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
-    FD_ZERO(&fds);
-    FD_SET(STDIN_FILENO, &fds); //STD_FILENO is 0
-    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
-    return FD_ISSET(STDIN_FILENO, &fds);
+int counter();
+void printTerm();
+int flag = 0;
+
+int main() {
+    int rep_amnt = 0, sesh_length = 0, brk_amnt = 0;
+
+    printf("How many reps, how long the sesh and break: ");
+    scanf("%d %d %d", &rep_amnt, &sesh_length, &brk_amnt);
+
+    system("mpv start_sound.wav > /dev/null 2>&1 & disown");
+    system("mpv start_sound.wav > /dev/null 2>&1 & disown");
+    counter(rep_amnt, sesh_length, brk_amnt);
+    system("mpv stop_sound.wav > /dev/null 2>&1 & disown");
+    system("clear");
+    return 0;
 }
 
-int hour=0;minute=0;second=0;flag=0;
-int cur_rep=0;rep_amnt=2;
+int counter(int rep, int sesh, int brk) {
+    int cur_rep = 0; int min = 0, sec = 0;
+    while (cur_rep != rep) {
+        time_t now = time(NULL);
 
-void delay(int ms) {
-    clock_t timeDelay = ms + clock();
-    while(timeDelay > clock());
-}
-
-int counter() {
-    while(!kbhit() && (cur_rep) != rep_amnt) {
-        if (minute > 50 && flag == 0) {
-            minute = 0;
+        if (min >= sesh && flag == 0) {
+            min = 0;
             flag = 1;
-            system("mpv bell_sound.wav > /dev/null 2>&1 & disown ");
-        } else if (minute > 10 && flag == 1) {
+            system("mpv bell_sound.wav > /dev/null 2>&1 & disown");
+        } else if (min >= brk && flag == 1) {
             ++cur_rep;
-            minute = 0;
+            min = 0;
             flag = 0;
-            if ((cur_rep) != rep_amnt) { system("mpv start_sound.wav > /dev/null 2>&1 & disown"); }
+            if (cur_rep != rep) { system("mpv start_sound.wav >/dev/null 2>&1 & disown"); }
         }
-        if (second > 59) {
-            second = 0;
-            ++minute;
+        if (sec > 59) {
+            sec = 0;
+            ++min;
         }
-        printData();
-        delay(1000000);
-        ++second;
+        while (now - time(NULL) == 0) { }
+        printTerm(cur_rep, min, sec);
+        ++sec;
     }
 }
 
-int printData() {
+void printTerm(int cur_rep, int min, int sec) {
     system("clear");
     if (flag == 0)
         printf("       Study %d\n", cur_rep + 1);
     else
         printf("         Break %d\n", cur_rep + 1);
-    printf("===============\n");
-    printf(" %d:%d:%d",hour,minute,second);
-    printf("\n===============\n");
-}
-
-int main() {
-    system("mpv start_sound.wav > /dev/null 2>&1 & disown");
-    counter();
-    system("mpv stop_sound.wav > /dev/null 2>&1 & disown");
-    system("clear");
-    printf("good sesh lad\n");
+    printf("================\n");
+    printf("\t%.2d:%.2d", min, sec);
+    printf("\n================\n");
 }
